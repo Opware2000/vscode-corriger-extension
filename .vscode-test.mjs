fcbdcd006e0931e8d238525be1c1c5c60651a7e5
Nicolas OGIER <Opware2000@users.noreply.github.com>
2026-01-16 23:00:14 +0100

    Correction de la configuration des tests pour séparer unitaires et E2E

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,5 +1,13 @@
 import { defineConfig } from '@vscode/test-cli';
 
-export default defineConfig({
-	files: 'out/test/**/*.test.js',
-});
+export default defineConfig([
+	{
+		label: 'unit',
+		files: 'out/test/**/*.test.js',
+		exclude: 'out/test/**/*.e2e.test.js',
+	},
+	{
+		label: 'e2e',
+		files: 'out/test/**/*.e2e.test.js',
+	},
+]);
